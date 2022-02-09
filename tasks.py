8f561cf6d0f4c3b155c48a5864c0f227f3a41aa5
Travis Nickles <ryochan7@protonmail.com>
2022-02-09 13:06:27 -0600

    Added invoke usage to project

---
+++ tasks.py
@@ -0,0 +1,11 @@
+from invoke import task
+
+@task
+def start_project(ctx):
+    """Start new screen session for mygpo projec"""
+    ctx.run("screen -dmS mygpo ./startapp_mygpo.sh")
+
+@task
+def stop_project(ctx):
+    """Add ^C to session console. Causes shell script to kill subprocesses"""
+    ctx.run("""screen -X -S mygpo stuff "^C" """)
